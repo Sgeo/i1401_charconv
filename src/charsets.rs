4f410ce941c43e5975a6284038e6ed9f25cc0cea
Sgeo <sgeoster@gmail.com>
2025-10-26 02:34:51 -0400

    Started adding SIMH, and there's lots of confusion

--- src/charsets.rs
+++ src/charsets.rs
@@ -1,6 +1,6 @@
 // Each character set is defined as an array mapping from internal BCD to external representation
 
-use std::sync::LazyLock;
+use std::{collections::HashMap, sync::LazyLock};
 
 const UNICODE_CARD_FROM_BCD: LazyLock<[char; 64]> = LazyLock::new(|| {
     // Taken from https://bitsavers.org/pdf/ibm/1401/A24-1403-5_1401_Reference_Apr62.pdf page 170
@@ -75,6 +75,16 @@ const UNICODE_CARD_FROM_BCD: LazyLock<[char; 64]> = LazyLock::new(|| {
     array
 });
 
+const BCD_FROM_UNICODE_CARD: LazyLock<HashMap<char, u8>> = LazyLock::new(|| {
+    let mut hashmap: HashMap<char, u8> = HashMap::with_capacity(64);
+    for (bcd, char) in UNICODE_CARD_FROM_BCD.iter().enumerate() {
+        hashmap.insert(*char, bcd as u8);
+    }
+    hashmap.insert(' ', 0); // Not relevant for unicode-card, but relevant for similar
+
+    hashmap
+});
+
 const UNICODE_PRINT_A_FROM_BCD: LazyLock<[char; 64]> = LazyLock::new(|| {
     let mut array = *UNICODE_CARD_FROM_BCD;
 
@@ -113,8 +123,96 @@ const VDC_FROM_BCD: LazyLock<[char; 64]> = LazyLock::new(|| {
     array[0b111010] = '?';
     array[0b111100] = 'q'; 
     array[0b111111] = 'g';
+    array[30] = '\\'; // Weird: Both VDC and SIMH have this as backslash, the reference calls it apostrophe
+    // Using the chart on https://rolffson.de/m_doc.html more directly is a good idea
+    // Only doing it partially, I think the rest of the chart matches, but not certain
+    array[62] = '<';
+    array[45] = ']';
+    array[46] = ';';
+    array[14] = '>';
 
     array
 
 });
 
+const BCD_FROM_VDC: LazyLock<HashMap<char, u8>> = LazyLock::new(|| {
+    let mut hashmap: HashMap<char, u8> = HashMap::with_capacity(64);
+    for (bcd, char) in VDC_FROM_BCD.iter().enumerate() {
+        hashmap.insert(*char, bcd as u8);
+    }
+    hashmap.insert(' ', 0); // Not relevant for unicode-card, but relevant for similar
+
+    hashmap
+});
+
+const SIMH_NEW_FROM_BCD: LazyLock<[char; 64]> = LazyLock::new(|| {
+    let mut array = ['�'; 64];
+
+    // Copy/pasted from SIMH documentation
+    array[0o00] = ' ';
+    array[0o01] = '1';
+    array[0o02] = '2';
+    array[0o03] = '3';
+    array[0o04] = '4';
+    array[0o05] = '5';
+    array[0o06] = '6';
+    array[0o07] = '7';
+    array[0o10] = '8';
+    array[0o11] = '9';
+    array[0o12] = '0';
+    array[0o13] = '#'; // Or =
+    array[0o14] = '@'; // Or '
+    array[0o15] = ':';
+    array[0o16] = '>';
+    array[0o17] = '{';
+    array[0o20] = '^';
+    array[0o21] = '/';
+    array[0o22] = 'S';
+    array[0o23] = 'T';
+    array[0o24] = 'U';
+    array[0o25] = 'V';
+    array[0o26] = 'W';
+    array[0o27] = 'X';
+    array[0o30] = 'Y';
+    array[0o31] = 'Z';
+    array[0o32] = '|';
+    array[0o33] = ',';
+    array[0o34] = '%'; // Or (
+    array[0o35] = '~';
+    array[0o36] = '\\'; // This is weird. Everything else has it as '
+    array[0o37] = '"';
+    array[0o40] = '-';
+    array[0o41] = 'J';
+    array[0o42] = 'K';
+    array[0o43] = 'L';
+    array[0o44] = 'M';
+    array[0o45] = 'N';
+    array[0o46] = 'O';
+    array[0o47] = 'P';
+    array[0o50] = 'Q';
+    array[0o51] = 'R';
+    array[0o52] = '!';
+    array[0o53] = '$';
+    array[0o54] = '*';
+    array[0o55] = ']';
+    array[0o56] = ';';
+    array[0o57] = '_';
+    array[0o60] = '&';
+    array[0o61] = 'A';
+    array[0o62] = 'B';
+    array[0o63] = 'C';
+    array[0o64] = 'D';
+    array[0o65] = 'E';
+    array[0o66] = 'F';
+    array[0o67] = 'G';
+    array[0o70] = 'H';
+    array[0o71] = 'I';
+    array[0o72] = '?';
+    array[0o73] = '.';
+    array[0o74] = ')';
+    array[0o75] = '[';
+    array[0o76] = '<';
+    array[0o77] = '}';
+
+    array
+});
